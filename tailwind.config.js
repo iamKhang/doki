d66401132348151c42aba266fafc5aaf32701b97
Nguyen Thanh Canh <thanhcanh.dev@gmail.com>
2024-09-10 21:32:52 +0700

    Cấu hình lại gluestack + nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ const gluestackPlugin = require("@gluestack-ui/nativewind-utils/tailwind-plugin"
 module.exports = {
   darkMode: process.env.DARK_MODE ? process.env.DARK_MODE : "media",
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./src/core-components/**/**/*.{html,js,jsx,ts,tsx}",
-    "./src/components/**/*.{html,js,jsx,ts,tsx,mdx}",
-    "./src/hooks/**/*.{html,js,jsx,ts,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{html,js,jsx,ts,tsx,mdx}",
+    "./hooks/**/*.{html,js,jsx,ts,tsx,mdx}",
   ],
+
   presets: [require("nativewind/preset")],
   safelist: [
     {
