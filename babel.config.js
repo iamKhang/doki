1176aaf12a87a84ac05308e7be02ce05b068d81f
thanhcanhit <thanhcanh.dev@gmail.com>
2024-11-27 20:50:33 +0700

    Fix svg import

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = function (api) {
           jsxImportSource: "nativewind",
         },
       ],
+      "@react-native/babel-preset",
       "nativewind/babel",
     ],
 
--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,17 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
+
 // Điều chỉnh cấu hình transformer
 config.transformer = {
   // Kết hợp với transformer mặc định
   ...config.transformer,
   // Thêm đường dẫn tới react-native-svg-transformer
   babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  assetPlugins: ["expo-asset/tools/hashAssetFiles"],
 };
 
 // Điều chỉnh cấu hình resolver
